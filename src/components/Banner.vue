f3784a7f99d5f752754ed588de91a46169585091
HNT <1090687607@qq.com>
2020-05-04 21:11:00 +0800

    新增切换项目

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -22,6 +22,37 @@
                      请输入项目名：<a-input style="width: 300px" placeholder="" />
                   </a-modal>
                </a-menu-item>
+
+               <a-menu-divider />
+               <a-menu-item key="0" @click="showExchangeModal">
+                  切换项目
+                  <a-modal v-model="exchangeVisible" title="切换项目" on-ok="handleOk">
+                     <template slot="footer">
+                     <a-button key="back" @click="exchangeHandleCancel">
+                        取消
+                     </a-button>
+                     <a-button key="submit" type="primary" :loading="exchangeLoading" @click="exchangeHandleOk">
+                        切换
+                     </a-button>
+                     </template>
+                     请选择要切至的项目：
+                     <a-radio-group v-model="value" @change="onChange">
+                        <a-radio :value="1">
+                        A
+                        </a-radio>
+                        <a-radio :value="2">
+                        B
+                        </a-radio>
+                        <a-radio :value="3">
+                        C
+                        </a-radio>
+                        <a-radio :value="4">
+                        D
+                        </a-radio>
+                     </a-radio-group>
+                  </a-modal>
+               </a-menu-item>
+
                <a-menu-divider />
                <a-menu-item key="1" @click="showDeleteConfirm">
                   删除项目
@@ -65,7 +96,10 @@ export default {
       return {
 			infoVisible: false,
 			createVisible: false,
-			createLoading: false,
+         createLoading: false,
+         exchangeVisible: false,
+         exchangeLoading: false,
+         value: 1,
 		};
 	},
 	methods: {
@@ -97,6 +131,25 @@ export default {
       createHandleCancel(e) {
          this.createVisible = false;
       },
+
+      //切换项目部分
+      showExchangeModal() {
+         this.exchangeVisible = true;
+      },
+      exchangeHandleOk(e){
+         this.exchangeLoading = true;
+         setTimeout(() => {
+            this.exchangeVisible = false;
+            this.exchangeLoading = false;
+         }, 2000);
+      },
+      exchangeHandleCancel(e) {
+         this.exchangeVisible = false;
+      },
+      onChange(e) {
+         console.log('radio checked', e.target.value);
+      },
+ 
 
       //删除项目部分
       showDeleteConfirm() {

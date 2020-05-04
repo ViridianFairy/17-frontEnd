dc4f38e2a4747ef81e9b90247d320f8b85c66377
heng1999 <1344434624@qq.com>
2020-05-04 16:58:11 +0800

    已完成未完成

--- src/components/TaskPanel.vue
+++ src/components/TaskPanel.vue
@@ -1,7 +1,7 @@
 <template>
    <div id="wrapper">
       <div id="pending">
-         <h2>待处理</h2>
+         <h2>未完成</h2>
          <div>
             <div>
                <a-button block @click="$router.push('/taskDetails')">软件工程实践</a-button>
@@ -205,22 +205,201 @@
             </a-collapse>
          </div>
       </div>
-      <div id="process">
-         <h2>进行中</h2>
-         <div>
-            <a-collapse accordion>
-               <a-collapse-panel header="✚" key="2" :showArrow="false">
-                  <p>{{ text }}</p>
-               </a-collapse-panel>
-            </a-collapse>
-         </div>
-      </div>
       <div id="finish">
          <h2>已完成</h2>
          <div>
             <a-collapse accordion>
-               <a-collapse-panel header="✚" key="3" :showArrow="false">
-                  <p>{{ text }}</p>
+               <a-collapse-panel header="✚" key="2" :showArrow="false">
+                  <a-input placeholder="输入任务标题" />
+                  <!-- 菜单  -->
+                  <p style="margin-top:20px">
+                     <a-avatar :size="30" icon="user" />
+                     <em style="font-style:normal;font-family:'Microsoft YaHei';color:gray;font-size:15px;margin-left:14.5px">用户</em>
+                  </p>
+                  <!-- 起止日期 -->                 
+                  <a-icon type="calendar" style="fontSize:20px;color:gray;margin-left:4px;margin-top:4px;vertical-align:bottom" />
+                     <!--a-date-picker
+                        style="margin-top:-10px;margin-bottom:5px;margin-left:18px;"
+                        :disabledDate="disabledStartDate"
+                        showTime
+                        format="YYYY-MM-DD HH:mm:ss"
+                        v-model="startValue"
+                        placeholder="开始时间"
+                        @openChange="handleStartOpenChange"
+                     /><br />
+                  <a-icon type="calendar" style="fontSize:20px;color:gray;margin-left:4px" />
+                     <a-date-picker
+                        style="margin-top:-10px;margin-bottom:5px;margin-left:18px;"
+                        :disabledDate="disabledEndDate"
+                        showTime
+                        format="YYYY-MM-DD HH:mm:ss"
+                        placeholder="结束时间"
+                        v-model="endValue"
+                        :open="endOpen"
+                        @openChange="handleEndOpenChange"
+                     /-->
+                     <a-date-picker style="color:gray;font-size:15px;margin-left:20px" placeholder="设置开始时间" @change="onChange1" @ok="onOk">
+                        <span>{{ dateString1 ? dateString1 : '设置开始时间' }}</span>
+                     </a-date-picker><br />
+                     <p style="margin-top:-20px;display:inline">
+                     <a-icon type="calendar" style="fontSize:20px;color:gray;margin-left:4px;margin-top:20px;vertical-align:bottom;margin-bottom:-0.1px" />
+                     
+                     <a-date-picker style="color:gray;font-size:15px;margin-left:20px" placeholder="设置截止时间" @change="onChange2" @ok="onOk">
+                        <span>{{ dateString2 ? dateString2 : '设置截止时间' }}</span>
+                     </a-date-picker>
+                     
+                  <!-- 提醒   -->
+                  <br />
+                  
+                     <a-icon type="clock-circle" style="fontSize:20px;color:gray;margin-left:4px;margin-top:22px;vertical-align:bottom;margin-bottom:0.2px" />                     
+                     
+                        <!--em class="em11" style="padding-left:20px;margin-bottom:60px" v-if="clocktype==0" @click="showModal">不提醒</em>
+                        <em class="em11" style="padding-left:20px" v-if="clocktype==1" @click="showModal">任务开始时</em>
+                        <em class="em11" style="padding-left:20px" v-if="clocktype==2" @click="showModal">任务截止时</em-->
+                     
+                  <p style="padding-left:20.5px;display:inline;color:gray;font-family:'Microsoft YaHei';font-size:15px" v-if="clocktype==0" @click="showModal">不提醒</p>
+                  <p style="padding-left:20.5px;display:inline;color:gray;font-family:'Microsoft YaHei';font-size:15px" v-if="clocktype==1" @click="showModal">任务开始时</p>
+                  <p style="padding-left:20.5px;display:inline;color:gray;font-family:'Microsoft YaHei';font-size:15px" v-if="clocktype==2" @click="showModal">任务截止时</p>
+
+                  <a-modal
+                     title="任务提醒设置"
+                     :visible="visible"
+                     :confirm-loading="confirmLoading"
+                     @ok="handleOk"
+                     @cancel="handleCancel"
+                     cancelText="取消"
+                     okText="保存"
+                     width="600px"
+                  >                           
+                  <p style="font-size:16px">
+                     提醒时间
+                     <a @click="showclock=1">
+                     <a-icon type="plus-circle" theme="filled" style="fontSize:18px;color:#003366;margin-left:370px;"/>                            
+                     添加新提醒
+                     </a>
+                  </p>
+                     <a-select v-if="showclock==1" style="width:160px;margin-top:-5px" @change="handleChange">
+                        <a-select-option value="begin" @click="clocktype=1">
+                           任务开始时
+                        </a-select-option>
+                        <a-select-option value="end" @click="clocktype=2">
+                           任务截止时
+                        </a-select-option>
+                        <a-select-option value="no" @click="clocktype=0">
+                           不提醒
+                        </a-select-option>
+                     </a-select>
+                     <p><br /></p>
+                  <p style="font-size:16px;margin-top:-15px">提醒对象</p>
+                     <a-avatar :size="35" icon="user" style="margin-top:-20px" />              
+                        <a-tooltip>
+                           <template slot="title">添加提醒对象</template>                   
+                           <a>
+                              <a-icon type="plus-circle" theme="filled" style="margin-top:-8px;fontSize:30px;color:#003366;margin-left:10px;"/><br />
+                           </a>
+                        </a-tooltip>
+                  </a-modal>         
+               
+                  <!-- 优先级   -->
+                  <a-menu style="width: 340px;margin-left:10x;margin-right:-20px">                     
+                     <a-sub-menu key="priority" style="margin-top:8px">
+                     <span slot="title">
+                        <a-icon type="fire" style="fontSize:20px;color:gray;margin-left:-12px;vertical-align:middle;margin-bottom:4px"/>
+                        <a-tag v-if="taskpriority=='较低'" color="gray" :visible="true" style="font-size:15px;height:22px;margin-left:10px;margin-top:14px">
+                           较低
+                        </a-tag> 
+                        <a-tag v-if="taskpriority=='普通'" color="green" :visible="true" style="font-size:15px;height:22px;margin-left:10px;margin-top:14px">
+                           普通
+                        </a-tag> 
+                        <a-tag v-if="taskpriority=='紧急'" color="orange" :visible="true" style="font-size:15px;height:22px;margin-left:10px;margin-top:14px">
+                           紧急
+                        </a-tag> 
+                        <a-tag v-if="taskpriority=='非常紧急'" color="red" :visible="true" style="font-size:15px;height:22px;margin-left:10px;margin-top:14px">
+                           非常紧急
+                        </a-tag>                  
+                     </span>
+                        <a-menu-item key="jd" @click="taskpriority='较低'">
+                           <a-tag color="gray" style="font-size:15px;height:22px;margin-left:0px">
+                              较低
+                           </a-tag>
+                        </a-menu-item>   
+                        <a-menu-item key="pt" @click="pt">
+                           <a-tag color="green" style="font-size:15px;height:22px;margin-left:0px">
+                              普通
+                           </a-tag>
+                        </a-menu-item>   
+                        <a-menu-item key=jj @click="jj">
+                           <a-tag color="orange" style="font-size:15px;height:22px;margin-left:0px">
+                              紧急
+                           </a-tag>
+                        </a-menu-item>
+                        <a-menu-item key=fc @click="fc">
+                           <a-tag color="red" style="font-size:14px;height:22px;margin-left:0px">
+                              非常紧急
+                           </a-tag>  
+                        </a-menu-item>    
+                     </a-sub-menu>
+                  </a-menu>
+
+                  <!--  标签   -->               
+                  <a-icon type="tag" style="fontSize:20px;color:gray;margin-left:4px;margin-top:14px;vertical-align:bottom;margin-bottom:0.3px" />
+
+                  <em class="em11" style="margin-left:11.5px;font-size:15px" @click="showlabel=1">标签</em>
+                  <div id="tags" v-if="showlabel==1">
+                     <div style="margin-left:4px;margin-top:3px">
+                        <template v-for="(tag, index) in tags">
+                           <a-tooltip v-if="tag.length > 20" :key="tag" :title="tag">
+                              <a-tag
+                                 :key="tag"
+                                 :closable="index !== 0"
+                                 @close="() => handleClose(tag)"
+                              >{{ `${tag.slice(0, 20)}...` }}</a-tag>
+                           </a-tooltip>
+                           <a-tag
+                              v-else
+                              :key="tag"
+                              :closable="index !== 0"
+                              color="#003366"
+                              style="font-size:14px;text-align:center;height:22px"
+                              @close="() => handleClose(tag)"
+                           >{{ tag }}</a-tag>
+                        </template>
+                        <a-input
+                           v-if="inputVisible"
+                           ref="input"
+                           type="text"
+                           size="default"
+                           :style="{ width: '78px' }"
+                           :value="inputValue"
+                           @change="handleInputChange"
+                           @blur="handleInputConfirm"
+                           @keyup.enter="handleInputConfirm"
+                        />
+                        <a-tag
+                           v-else
+                           @click="showInput"
+                           style="background:#fff;height:22px; borderStyle: dashed;margin-top:8px"
+                        >
+                           <a-icon type="plus" />
+                           <em style="font-size:14px;font-style:normal;font-family:'Microsoft YaHei'">添加标签</em>
+                        </a-tag>
+                     </div>
+                  </div>
+                  <a-divider style="margin-top:15px"/>
+                  <p style="margin-top:-15px;font-size:16px;margin-left:5px">参与者 · 1</p>                
+                     <a-avatar :size="35" icon="user" style="margin-top:-18px" />
+                     <a-tooltip>
+                        <template slot="title">
+                           添加参与者
+                        </template>                   
+                        <a>
+                           <a-icon type="plus-circle" theme="filled" style="fontSize:30px;color:#003366;margin-left:10px;"/><br />
+                        </a>
+                     </a-tooltip>
+                  <a-button style="font-size:16px;float:right">创建</a-button>
+
+                  <br />
+                  <br />             
                </a-collapse-panel>
             </a-collapse>
          </div>

2e36013edb4a7232e8b5c32c9501c00991d844f5
HNT <1090687607@qq.com>
2020-05-30 22:31:15 +0800

    主页面新建项目按钮

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,22 @@
 <template>
    <div>
         <div id="add-button">
-            <a-button>新建项目</a-button>
+            <a-button @click="showCreateModal">新建项目</a-button>
+            <a-modal v-model="createVisible" title="创建新项目" on-ok="handleOk">
+                <template slot="footer">
+                    <a-button key="back" @click="createHandleCancel">取消</a-button>
+                    <a-button
+                        key="submit"
+                        type="primary"
+                        :loading="createLoading"
+                        @click="createHandleOk"
+                    >创建</a-button>
+                </template>
+                    请输入项目名：
+                <a-input style="width: 300px" placeholder v-model="newName" />
+            </a-modal>
         </div>
+        
         <div id="join-text">已加入项目</div>
    </div>
 </template>
@@ -13,13 +27,34 @@ export default {
    components: {},
    data() {
       return {
-
+        createVisible: false,
+        createLoading: false,
+        newName: "",
       };
 	},
    mounted(){
    },
    methods: {
-		
+    showCreateModal() {
+        this.createVisible = true;
+    },
+    createHandleOk(e) {
+        this.createLoading = true;
+        this.$http.post(`/api/project`, { name: this.newName }).then(doc => {
+            var code = doc.data.status;
+            var msg = doc.data.msg;
+            if (code != 0) {
+               this.$alert(msg, "false");
+               this.createLoading = false;
+            } else {
+               this.createVisible = false;
+               this.createLoading = false;
+            }
+        });
+    },
+    createHandleCancel(e) {
+        this.createVisible = false;
+    },
    },
 };
 </script>
